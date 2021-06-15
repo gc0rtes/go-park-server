52cbb4b659bb1572e9a24000fd98f1ce28a32449
gc0rtes <guilhermeco@gmail.com>
2021-06-15 11:33:19 +0200

    Create user model. Add constrains to fields.

--- list-of-create-models-commands.md
+++ list-of-create-models-commands.md
@@ -1,3 +1,6 @@
 npx sequelize-cli model:generate --name city --attributes imageUrl:string,name:string
 
 npx sequelize-cli model:generate --name park --attributes imageUrl:string,name:string,lat:float,lng:float,cityId:integer
+
+npx sequelize-cli model:generate --name user --attributes imageUrl:string,name:string,email:string,password:string
+
---
+++ migrations/20210615092731-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+       ​ unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
