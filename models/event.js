e4231dcd88616b9224edafe6970c18b618110d5b
gc0rtes <guilhermeco@gmail.com>
2021-06-15 16:38:29 +0200

    Setup association event - tagEvent - tag

--- models/event.js
+++ models/event.js
@@ -4,6 +4,10 @@ module.exports = (sequelize, DataTypes) => {
   class event extends Model {
     static associate(models) {
       event.belongsTo(models.park);
+      event.belongsToMany(models.tag, {
+        through: "tagEvents",
+        foreignKey: "eventId",
+      });
     }
   }
   event.init(
