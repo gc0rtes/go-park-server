5b107063f28ba7b8c89f5e34bc47f65d7e876dc0
gc0rtes <guilhermeco@gmail.com>
2021-06-17 08:53:10 +0200

    Query done

--- query-relations.js
+++ query-relations.js
@@ -1,6 +1,7 @@
 const {
   user,
   event,
+  tag,
   tagEvent,
   commentEvent,
   attendenceEvent,
@@ -9,27 +10,48 @@ const {
 } = require("./models");
 
 async function listCityWithParks() {
-  const lists = await city.findAll({
-    include: { model: park, attributes: ["name"] },
+  const result = await city.findAll({
+    include: [
+      { model: park, attributes: ["name"] },
+      // { model: event, attributes: ["title"] }, // event is not associated ti city.
+    ],
   });
 
-  return lists.map((list) => list.get({ plain: true }));
+  return result.map((list) => list.get({ plain: true }));
 }
 
 // listCityWithParks().then((lists) => console.log(lists));
 
-async function parkWithCities() {
-  const lists = await park.findAll({
-    include: { model: city, attributes: ["name"] },
+// Find events by city
+async function findEventsByCity() {
+  const result = await event.findAll({
+    attributes: ["title", "description"],
+    include: {
+      model: park,
+      attributes: ["name", "cityId"],
+      where: { cityId: 1 },
+    },
   });
 
-  return lists.map((list) => list.get({ plain: true }));
+  return result.map((list) => list.get({ plain: true }));
 }
-// parkWithCities().then((lists) => console.log(lists));
+
+// findEventsByCity().then((lists) => console.log(lists));
+
+async function parkWithCityandEvents() {
+  const result = await park.findAll({
+    include: [
+      { model: city, attributes: ["name"] },
+      { model: event, attributes: ["title"] },
+    ],
+  });
+
+  return result.map((list) => list.get({ plain: true }));
+}
+// parkWithCityandEvents().then((lists) => console.log(lists));
 
 async function getUserWithEventOwner(id) {
   const result = await event.findByPk(id, {
-    // include: [{ model: user, as: "going" }],
     include: [
       { model: user, as: "owner" },
       { model: user, as: "going" },
@@ -39,4 +61,40 @@ async function getUserWithEventOwner(id) {
   return result.get({ plain: true });
 }
 
-getUserWithEventOwner(2).then((user) => console.log(user));
+// getUserWithEventOwner(2).then((user) => console.log(user));
+
+async function eventByTag() {
+  const result = await event.findAll({
+    include: { model: tag },
+  });
+  return result.map((list) => list.get({ plain: true }));
+}
+
+// eventByTag().then((user) => console.log(user));
+
+async function eventByTag() {
+  const result = await tag.findAll({
+    where: { name: "game" },
+    include: { model: event, attributes: ["title"] },
+  });
+  return result.map((list) => list.get({ plain: true }));
+}
+
+// eventByTag().then((user) => console.log(user));
+
+async function eventsSortedTime() {
+  const result = await event.findAll({ order: [["startDate", "ASC"]] });
+  return result.map((list) => list.get({ plain: true }));
+}
+
+// eventsSortedTime().then((user) => console.log(user));
+
+// What events One user is going?  how many events? -> going.map
+async function userGoingEvents(id) {
+  const result = await user.findByPk(id, {
+    include: [{ model: event, as: "going" }],
+  });
+  return result.get({ plain: true });
+}
+
+userGoingEvents(2).then((user) => console.log(user));
